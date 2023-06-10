bca2f7cc46aa81bbd4c07691681204ba39e62802
Bryann Torres <arleyro91@gmail.com>
2023-06-10 13:33:34 -0500

    Added link and meta tags to root file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
+import Counter from "~/components/starter/counter/counter";
+import Hero from "~/components/starter/hero/hero";
+import Infobox from "~/components/starter/infobox/infobox";
+import Starter from "~/components/starter/next-steps/next-steps";
 
 export default component$(() => {
   return (
@@ -58,7 +58,7 @@ export default component$(() => {
               Example Apps
             </div>
             <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{' '}
+              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
               <a href="/demo/todolist">Todo App</a>.
             </p>
           </Infobox>
@@ -102,11 +102,75 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Arleyro | Portfolio",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content:
+        "Web development portfolio of Bryann Torres. Check out my web development projects that highlight my skills and experience. Also find my updated CV for a detailed view of my professional journey.",
+    },
+    {
+      name: "author",
+      content: "Bryann Torres",
+    },
+    {
+      name: "theme-color",
+      content: "#000000",
+    },
+    {
+      name: "background-color",
+      content: "#ffffff",
+    },
+    {
+      property: "og:title",
+      content: "Arleyro | Portfolio",
+    },
+    {
+      property: "og:description",
+      content:
+        "Web development portfolio of Bryann Torres. Check out my web development projects that highlight my skills and experience. Also find my updated CV for a detailed view of my professional journey.",
+    },
+    {
+      property: "og:image",
+      content: "https://arleyro.com/logo256.png",
+    },
+    {
+      property: "og:url",
+      content: "https://arleyro.com",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+    {
+      property: "og:site_name",
+      content: "Arleyro",
+    },
+  ],
+  links: [
+    {
+      rel: "icon",
+      href: "/favicon/favicon.svg",
+      sizes: "any",
+      type: "image/svg+xml",
+    },
+    {
+      rel: "icon",
+      href: "/favicon/favicon-32x32.ico",
+      sizes: "32x32",
+      type: "image/x-icon",
+    },
+    {
+      rel: "icon",
+      href: "/favicon/favicon-64x64.ico",
+      sizes: "64x64",
+      type: "image/x-icon",
+    },
+    {
+      rel: "icon",
+      href: "/favicon/favicon-256x256.ico",
+      sizes: "256x256",
+      type: "image/x-icon",
     },
   ],
 };
