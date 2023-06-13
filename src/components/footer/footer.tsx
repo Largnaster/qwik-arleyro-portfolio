92712c908686affbf4436f7d5d4a0cbeb442419d
Bryann Torres <arleyro91@gmail.com>
2023-06-12 23:14:14 -0500

    Moved footer styling to global styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import { useServerTimeLoader } from "~/routes/layout";
-import styles from "./footer.module.css";
 
 export default component$(() => {
   const serverTime = useServerTimeLoader();
@@ -8,20 +7,12 @@ export default component$(() => {
   return (
     <footer>
       <div class="container">
-        <a
-          href="https://qwik.builder.io/"
-          target="_blank"
-          class={styles.anchor}
-        >
+        <a href="https://qwik.builder.io/" target="_blank" class="anchor">
           <span>Made with Qwik</span>
         </a>
-        <a
-          href="https://github.com/Largnaster/"
-          target="_blank"
-          class={styles.anchor}
-        >
+        <a href="https://github.com/Largnaster/" target="_blank" class="anchor">
           <span>By Largnaster</span>
-          <span class={styles.spacer}>|</span>
+          <span class="spacer">|</span>
           <span>{serverTime.value.date}</span>
         </a>
       </div>
