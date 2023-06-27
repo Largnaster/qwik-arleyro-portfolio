cbed3578b0b9a7c12580eecdbecf9be3b72d694d
Bryann Torres <arleyro91@gmail.com>
2023-06-26 21:20:58 -0500

    Updated card to use slot

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -1,35 +1,17 @@
-import { JSXNode, component$ } from "@builder.io/qwik";
-import { Fragment } from "@builder.io/qwik/jsx-runtime";
-
-interface FooterItem {
-  name: string;
-  component: JSXNode;
-}
+import { component$, Slot } from "@builder.io/qwik";
 
 interface CardProps {
   title: string;
   description: string;
   key?: number | string;
-  footerItems?: FooterItem[];
 }
 
-export default component$<CardProps>(
-  ({ title, description, key, footerItems }) => {
-    return (
-      <div key={key} class="card">
-        <h3>{title}</h3>
-        <div>{description}</div>
-        {footerItems && footerItems.length > 0 && (
-          <Fragment>
-            <hr />
-            <div class="card-footer">
-              {footerItems.map((item) => (
-                <div key={`footer_item_${item.name}`}>{item.component}</div>
-              ))}
-            </div>
-          </Fragment>
-        )}
-      </div>
-    );
-  }
-);
+export default component$<CardProps>(({ title, description, key }) => {
+  return (
+    <div key={key} class="card">
+      <h3>{title}</h3>
+      <div>{description}</div>
+      <Slot name="card-footer" />
+    </div>
+  );
+});
