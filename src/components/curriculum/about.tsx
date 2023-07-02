c74fc25e3cfd561cb774d53063d77d5298b1a732
Bryann Torres <arleyro91@gmail.com>
2023-07-02 17:58:24 -0500

    Added some ideas to 'about' section

--- src/components/curriculum/about.tsx
+++ src/components/curriculum/about.tsx
@@ -1,8 +1,22 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
 import { useTranslate } from "qwik-speak";
+import Chip from "../common/Chip";
 
 export default component$(() => {
   const t = useTranslate();
+
+  const ideasList = useStore([
+    "English",
+    "Spanish",
+    "Self-taught",
+    "Teamwork",
+    "Stress management",
+    "Programming",
+    "Gaming",
+    "Music",
+    "Cooking",
+    "Anime"
+  ]);
 
   return (
     <div>
@@ -10,6 +24,14 @@ export default component$(() => {
       <div>
         <div class="large">{t("app.about.location")}</div>
         <p>{t("app.about.myDescription")}</p>
+      </div>
+      <div>
+        <p class="lead pb-4">{t("app.about.ideas")}</p>
+        <div class="button-group">
+          {ideasList.map((idea) => (
+            <Chip key={`idea_chip_${idea}`} text={idea} />
+          ))}
+        </div>
       </div>
     </div>
   );
