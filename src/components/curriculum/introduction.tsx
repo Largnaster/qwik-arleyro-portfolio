ade9970d9d1af767ab70c8af9b76e09685025485
Bryann Torres <arleyro91@gmail.com>
2024-02-23 21:54:57 -0500

    feat: update introduction with daisy

--- src/components/curriculum/introduction.tsx
+++ src/components/curriculum/introduction.tsx
@@ -8,60 +8,88 @@ export default component$(() => {
   const t = inlineTranslate();
 
   const technologiesList = useStore([
-    "React",
-    "Django",
-    "Postgres",
-    "Python",
-    "TypeScript"
+    { label: "React", image: "/images/react-icon.svg" },
+    { label: "Django", image: "/images/django-icon.svg" },
+    { label: "Node.js", image: "/images/nodejs-icon.svg" },
+    { label: "Postgres", image: "/images/postgresql-icon.svg" },
+    { label: "Python", image: "/images/python-icon.svg" },
+    { label: "TypeScript", image: "/images/typescript-icon.svg" }
   ]);
-  const toolsList = useStore(["Git", "GitHub", "VSCode", "Docker", "Linux"]);
+  const toolsList = useStore([
+    { label: "Git", image: "/images/git-icon.svg" },
+    { label: "GitHub", image: "/images/github-icon.svg" },
+    { label: "VSCode", image: "/images/vscode-icon.svg" },
+    { label: "Docker", image: "/images/docker-icon.svg" }
+  ]);
   return (
-    <div id="introduction">
-      <div class="text-center flex flex-col space-y-2 pb-4">
-        <h1>{t("app.introduction.title")}</h1>
-        <h2 class="border-b-0">{t("app.introduction.subtitle")}</h2>
-        <button class="btn btn-primary">Button</button>
-        <div class="button-group justify-center items-center">
-          <Link
-            class="has-svg"
-            href="https://www.linkedin.com/in/arleyro91/"
-            target="_blank"
-            aria-label="LinkedIn icon"
-          >
-            <LuLinkedin class="icon" />
-          </Link>
-          <Link
-            class="has-svg"
-            href="mailto:arleyro91@gmail.com"
-            target="_blank"
-            aria-label="Mail icon"
-          >
-            <LuMail class="icon" />
-          </Link>
-          <Link
-            class="has-svg"
-            href="https://github.com/Largnaster"
-            target="_blank"
-            aria-label="GitHub icon"
-          >
-            <LuGithub class="icon" />
-          </Link>
+    <div class="text-center">
+      <div>
+        <div class="typo mx-auto">
+          <h1>{t("app.introduction.title")}</h1>
+          <h2>{t("app.introduction.subtitle")}</h2>
+        </div>
+        <div class="flex flex-row justify-center space-x-4 mx-auto my-3 w-full">
+          <div class="dui-tooltip dui-tooltip-bottom" data-tip="LinkedIn">
+            <Link
+              href="https://www.linkedin.com/in/arleyro91/"
+              target="_blank"
+              aria-label="LinkedIn icon"
+            >
+              <LuLinkedin class="h-6 w-6" />
+            </Link>
+          </div>
+          <div class="dui-tooltip dui-tooltip-bottom" data-tip="e-mail">
+            <Link
+              href="mailto:arleyro91@gmail.com"
+              target="_blank"
+              aria-label="Mail icon"
+            >
+              <LuMail class="h-6 w-6" />
+            </Link>
+          </div>
+          <div class="dui-tooltip dui-tooltip-bottom" data-tip="GitHub">
+            <Link
+              href="https://github.com/Largnaster"
+              target="_blank"
+              aria-label="GitHub icon"
+            >
+              <LuGithub class="h-6 w-6" />
+            </Link>
+          </div>
         </div>
       </div>
-      <div>
+      <div class="typo max-w-none text-left">
         <p>{t("app.introduction.aboutMe")}</p>
-        <p class="lead pb-4">{t("app.introduction.frameworks")}</p>
-        <div class="button-group">
-          {technologiesList.map((technology) => (
-            <Chip key={`technology_chip_${technology}`} text={technology} />
-          ))}
-        </div>
-        <p class="lead pb-4">{t("app.introduction.tools")}</p>
-        <div class="button-group">
-          {toolsList.map((tool) => (
-            <Chip key={`technology_chip_${tool}`} text={tool} />
-          ))}
-        </div>
+        <ul>
+          <li>
+            <p>
+              <strong>{t("app.introduction.frameworks")}</strong>
+            </p>
+            <div class="flex flex-row space-x-3">
+              {technologiesList.map((technology) => (
+                <Chip
+                  key={`technology_chip_${technology.label}`}
+                  text={technology.label}
+                  imageSrc={technology.image}
+                />
+              ))}
+            </div>
+          </li>
+          <li>
+            <p>
+              <strong>{t("app.introduction.tools")}</strong>
+            </p>
+            <div class="flex flex-row space-x-3">
+              {toolsList.map((tool) => (
+                <Chip
+                  key={`technology_chip_${tool.label}`}
+                  text={tool.label}
+                  imageSrc={tool.image}
+                />
+              ))}
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
