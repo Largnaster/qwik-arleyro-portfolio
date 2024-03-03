f70efba2e31743f5c2e451ca6cffbe37ed03d04c
Bryann Torres <arleyro91@gmail.com>
2024-03-03 12:43:04 -0500

    feat: fix styles of `contact.tsx`

--- src/components/curriculum/contact.tsx
+++ src/components/curriculum/contact.tsx
@@ -1,18 +1,36 @@
 import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
 import { inlineTranslate } from "qwik-speak";
 
 export default component$(() => {
   const t = inlineTranslate();
 
   return (
-    <div id="contact">
+    <div id="contact" class="typo-block">
       <h2>{t("app.contact.title")}</h2>
-      <div>
-        <p>{t("app.contact.description")}</p>
-      </div>
-      <div>
-        <a href="mailto:arleyro91@gmail.com">arleyro91@gmail.com</a>
-      </div>
+      <p>{t("app.contact.description")}</p>
+      <ul>
+        <li>
+          <Link
+            class="dui-link dui-link-hover dui-link-primary"
+            href="mailto:arleyro91@gmail.com"
+            target="_blank"
+            aria-label="My e-mail address"
+          >
+            arleyro91@gmail.com
+          </Link>
+        </li>
+        <li>
+          <Link
+            class="dui-link dui-link-hover dui-link-primary"
+            href="https://www.linkedin.com/in/arleyro91/"
+            target="_blank"
+            aria-label="My LinkedIn profile"
+          >
+            LinkedIn
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 });
